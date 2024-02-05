1df9cc565322e66f218e32b71511c12e1b5a6d5b
BISHAL parikh <bishal@s.amity.edu>
2024-02-05 10:17:29 +0000

    schema public table added

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -8,17 +8,17 @@ CREATE TABLE public.users(
     display_name text,
     handle text,
     cognito_user_id text,
-    created_at TIMESTAMP default current_timestamp NOT NULL
-
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
 
+
 CREATE TABLE public.activities (
-  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  message text NOT NULL,
-  replies_count integer DEFAULT 0,
-  reposts_count integer DEFAULT 0,
-  likes_count  integer DEFAULT 0,
-  reply_to_activity_uuid integer,
-  expires_at TIMESTAMP,
-  created_at TIMESTAMP deafult current_timestamp NOT NULL
+    uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    message text NOT NULL,
+    replies_count integer DEFAULT 0,
+    reposts_count integer DEFAULT 0,
+    likes_count integer DEFAULT 0,
+    reply_to_activity_uuid UUID,
+    expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
