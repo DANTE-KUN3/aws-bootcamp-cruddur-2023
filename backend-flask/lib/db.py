570e69b5bceee0333d1d20dd18ad2d99309b0de8
BISHAL parikh <bishal@s.amity.edu>
2024-02-08 18:59:07 +0000

    import pool error

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -2,5 +2,33 @@ from psycopg_pool import ConnectionPool
 import os
 
 
-connection_url=os.getenv("CONNECTION_URL")
-pool=ConnectionPool(connection_url)
+def query_wrap_object(template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+        {template}
+    ) object_row);
+    """
+    return sql
+
+def query_wrap_array(template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+        {template}
+    ) array_row);
+    """
+    return sql
+
+def get_connection():
+    connection_url = os.getenv("CONNECTION_URL")
+    pool = ConnectionPool(connection_url)
+    
+
+# Usage example:
+# conn = get_connection()
+# cursor = conn.cursor()
+# cursor.execute(query_wrap_objects('SELECT * FROM my_table'))
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row)
+# cursor.close()
+# pool.putconn(conn)
