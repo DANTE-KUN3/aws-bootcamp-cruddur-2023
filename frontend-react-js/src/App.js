d7ce90441fbcecb9e24a5eae58679a89d32b84f7
BISHAL parikh <bishal@s.amity.edu>
2024-02-16 07:55:27 +0000

    cognito fix

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -10,7 +10,7 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import {Amplify} from 'aws-amplify';
+
 
 import {
   createBrowserRouter,
