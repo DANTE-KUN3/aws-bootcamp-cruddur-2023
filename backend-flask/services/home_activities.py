cf38a6d54b3014c9caf3c7188e984699f8c6b811
BISHAL parikh <bishal@s.amity.edu>
2024-02-25 09:58:47 +0000

    til 35:22

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool,query_wrap_array
+from lib.db import db
 
 
 tracer = trace.get_tracer("home.activities")
@@ -14,7 +14,7 @@ class HomeActivities:
             #span.set_attribute("app.now",now.isoformat())
             #span.set_attribute("app.result_length",len(results))
 
-        sql = query_wrap_array('''
+        results=db.query_array_json("""
             SELECT 
                 activities.uuid,
                 users.display_name,
@@ -29,15 +29,5 @@ class HomeActivities:
             FROM public.activities
             LEFT JOIN public.users ON users.uuid=activities.user_uuid
             ORDER BY activities.created_at DESC
-        ''')
-        print("sql---------")
-        print(sql)
-        print("sql--------")
-
-        
-        with pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(sql)
-                json = cur.fetchone()
-       
-        return json[0]
+        """)
+        return results
